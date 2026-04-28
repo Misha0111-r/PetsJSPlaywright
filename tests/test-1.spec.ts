36e78ac0e9263bae08dd3404f9b6fecc67ffb99c
Misha0111-r <misha0111.ak@gmail.com>
2026-04-28 17:15:59 +0300

    Initial commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://34.141.58.52:8080/#/');
+  await page.getByRole('menuitem', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Login *' }).click();
+  await page.getByRole('textbox', { name: 'Login *' }).fill('Obama@gmail.com');
+  await page.locator('input[type="password"]').click();
+  await page.locator('input[type="password"]').fill('BarackObama');
+  await page.locator('html').click();
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page).toHaveURL('http://34.141.58.52:8080/#/profile');
+});
